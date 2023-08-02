077cffcaef16511198241634d5732f4e9171afc7
jenaalsup <jena.alsup@gmail.com>
2023-08-01 18:17:18 -0700

    add extended stop words and get correct filepath

--- Code/preprocess-sustainability-reports.py
+++ Code/preprocess-sustainability-reports.py
@@ -1,16 +1,15 @@
-# TODO: reorganize imports
-import numpy as np
-import pandas as pd
-import nltk
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
-import gensim
-import file_operations as fop
 import os
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import modin.pandas as md
+import file_operations as fop
 
 # constants
-ROOT_DIR        = "/reports" # raw data
+ROOT_DIR        = "./reports" # raw data
 INDIR           = "processed_reports/"
 RAW_DATA_PREFIX = "processed_reports/" # save the processed data
 
@@ -23,7 +22,7 @@ def basic_clean(df):
     df['speech'] = df['speech'].astype('str')
     df = df.drop_duplicates(keep="first")
     df['speech'] = df['speech'].str.lower()
-    df['speech'] = df['speech'].str.replace(r'[^\w\s\d]+', '')
+    df['speech'] = df['speech'].str.replace(r'[^\w\s\d]+', '') # TODO: what does this do?
     return df
 
 porter_stemmer = PorterStemmer()
@@ -32,9 +31,37 @@ def stem_sentence(sentence):
     stemmed_tokens = [porter_stemmer.stem(token) for token in tokens]
     return ' '.join(stemmed_tokens)
 
+inDir = os.path.join(ROOT_DIR, INDIR)
+dl = sorted(fop.get_files_in_dir(ROOT_DIR)) # put files in chronological order
+print(dl)
+
+for i, f in enumerate(dl):
+    path_in = os.path.join(inDir,f)
+    print(path_in)
+    # TODO: can read_csv work with txt files? - the following line causes an error
+    #df = md.read_csv(path_in, sep="|",encoding='cp1252',on_bad_lines='skip' ,engine="python" ,names = ['speech_id','speech'],usecols=['speech_id','speech'])
+
+# TODO: what does this for loop do?
+for i, f in enumerate(dl):
+    source_path = os.path.join(ROOT_DIR, RAW_DATA_PREFIX + f)
+    for j,chunk in enumerate(pd.read_csv(source_path, chunksize=200000)):
+        nrows = chunk.shape[0]
+        if nrows==200000:
+            chunk.to_csv(os.path.join(ROOT_DIR, RAW_DATA_PREFIX + Path(f).stem+'_'+str(j)+'.txt'), index=False) 
+        if j>0:
+            chunk.to_csv(os.path.join(ROOT_DIR, RAW_DATA_PREFIX + Path(f).stem+'_'+str(j)+'.txt'), index=False) 
+
+
+# TODO: where is the rest of the code being used?
+
 stop_words  = (stopwords.words('english'))
-#added_words = ["sustainability", "environment"] # TODO: tweak if necessary, go through danny's and keep important stuff
-#stop_words= list(np.append(stop_words,added_words))
+added_words = ["will","has","by","for","hi","hey","are","as","i","we","our","ours","ourselves","use",
+               "you","your","yours","that","f","e","s","t","c","n","u","v","l","p","d","b","g","k","m","x","y","z",
+               "be","with","is","was","been","not","they","way","and","to","do","go","on","have","from",
+               "at","but","or","an","if","all","so","it","thing","put","well","take","see","","can't","can",
+               "got","cant","could","him","his","this","had","he","her","she","hers","their","they're","things",
+               "go","going","let","would","make","like","come","us"]
+stop_words= list(np.append(stop_words,added_words))
 
 countvec = CountVectorizer( stop_words = stop_words,
                             lowercase = True,
@@ -42,7 +69,5 @@ countvec = CountVectorizer( stop_words = stop_words,
                             max_df = 10000, # remove words with > 10,000 occurrences
                             min_df = 20)# remove words with < 20 occurrencees
 
-inDir = os.path.join(ROOT_DIR, INDIR)
-# TODO: not working - get_files_in_dir() method not found
-dl = sorted(fop.get_files_in_dir(inDir)) # put files in chronological order
-print("Done. Split files located at: {}.\n".format(inDir))
+
+
