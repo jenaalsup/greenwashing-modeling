45cbf383184ad22e510913376890a28c66a2e050
jenaalsup <jena.alsup@gmail.com>
2023-08-15 14:20:17 -0700

    add doc_id column to csv

--- code/preprocess-sustainability-reports.py
+++ code/preprocess-sustainability-reports.py
@@ -33,7 +33,7 @@ def stem_sentence(sentence):
     return ' '.join(stemmed_tokens)
 
 df = pd.DataFrame()
-column_names = ['company-type', 'company-ticker', 'year', 'part', 'text']
+column_names = ['doc_id', 'company-type', 'company-ticker', 'year', 'part', 'text']
 dl = sorted(fop.get_files_in_dir(ROOT_DIR)) # sort files chronologically
 for i, f in enumerate(dl):
     # open file:
@@ -51,7 +51,8 @@ for i, f in enumerate(dl):
         company_type = 'energy'
     if (filename_parts[0].upper() in CLEAN_ENERGY_TICKERS):
         company_type = 'clean-energy'
-    data = {'company-type': company_type,
+    data = {'doc_id': f,
+            'company-type': company_type,
             'company-ticker': filename_parts[0],
             'year': filename_parts[1],
             'part': filename_parts[2][:-4],
