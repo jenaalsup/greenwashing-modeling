111e65c74012b8198a20b4a300725dd616d98055
jenaalsup <jena.alsup@gmail.com>
2023-08-17 00:36:29 -0700

    add capex data csv and updates from interpreting model

--- code/model-sustainability-reports.R
+++ code/model-sustainability-reports.R
@@ -1,5 +1,7 @@
 # load libraries
 library(tidytext)
+library(dplyr)
+library(tidyverse)
 library(quanteda)
 library(parallel)
 library(stm)
@@ -7,6 +9,8 @@ library(stm)
 # load data
 textData <- read.csv("reports/processed_reports/processed-data.csv")
 textData <- textData[!duplicated(textData$doc_id),]
+capex_data <- read_csv("reports/processed_reports/capex-data.csv")
+textData <- textData %>%left_join(capex_data)
 
 # prepare JST model
 attach(textData)
@@ -24,13 +28,13 @@ stop_words_custom = c("will","has","by","for","hi","hey","are","as","i","we","ou
 # create tokens for kept words
 toks1 <- tokens(corpus(llDisplay), remove_punct = TRUE)
 toks2 <- tokens_remove(toks1, stop_words_custom)
-toks3 <- tokens_ngrams(toks2, 1:3)
+toks3 <- tokens_ngrams(toks2, 1:2) # was 1:3
 toks3 <- tokens_wordstem(toks3)
 
 # create a document feature matrix (quanteda style) to run jst
 N = nrow(llDisplay)
 dfm_speeches <- dfm(toks3, remove_punct = TRUE, remove_numbers = TRUE, stem = T) %>% 
-  dfm_trim(min_termfreq = 25, min_docfreq =0.002*N, max_docfreq = 0.4*N)
+  dfm_trim(min_termfreq = 15, min_docfreq =0.01*N, max_docfreq = 0.8*N) # was 25, 0.002, 0.4
 
 dfm_speeches <- dfm_subset(dfm_speeches,ntoken(dfm_speeches) >= 1)
 
@@ -45,11 +49,12 @@ meta      <- houseDfmSTM$meta
 # select number of topics
 kk = c(5,10,15,20,25,28,30,40,50,60)
 
-# run stm
+# run stm with metadata on prevalence 
 model_stm <- mclapply(kk,
                       FUN = function(kk) 
                         stm(houseDfmSTM$documents, 
                             houseDfmSTM$vocab,
+                            prevalence=~factor(company_type) + factor(year),
                             K = kk,
                             max.em.its = 200,
                             data = houseDfmSTM$meta, 
@@ -57,4 +62,30 @@ model_stm <- mclapply(kk,
                             seed = 153332),
                             mc.cores = getOption("mc.cores", 5L))
 
-save(model_stm,file="modelOutput-House-STM.RData")
+save(model_stm,file="modelOutput-House-STM2.RData")
+
+
+labelTopics(model_stm[[2]])
+
+### Google how to compute perplexity and coherence, topic selection 
+
+# After topic selection
+model_stm[[1]]$theta # doc-topic matrix
+model_stm[[1]]$settings$covariates # covariates 
+
+## Once you're happy with the model and did topic selection, correlate the columns of theta(ie the topics) which are green with the financial data
+## Plos the scatter with the slope
+
+## Use ggplot scatter with regression slope (confidence interval)
+
+data_set_with_topics_and_metadata<- data_set_with_topics_and_metadata %>%group_by(ticker) %>%summarise_all(mean(topics)) 
+#Using PCA, plot the first two coordinates in a scatter (dim1,dim2) and color code by company type 
+
+# subset to fossil fuel companies
+lm(financial_1~topic1) 
+m(financial_1~topic1)
+m(financial_1~topick)
+lm(financial_1~topic1+..+topic_k)
+lm(financial_2~topic1+..+topic_k)
+
+
